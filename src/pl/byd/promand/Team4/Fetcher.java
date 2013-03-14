7eeff7dc61172515ae44deccc964f83fc1af239b
gytautaspilkauskas <gytautaspilkauskas@172.18.1.37>
2013-03-14 17:00:33 +0100

    Fetcher

---
+++ src/pl/byd/promand/Team4/Fetcher.java
@@ -0,0 +1,22 @@
+package pl.byd.promand.Team4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class Fetcher extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.header);
+	
+	}
+}
