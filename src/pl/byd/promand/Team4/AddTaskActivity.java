7a591d55405eebea37e41ad919edaa3578692303
gytautaspilkauskas <gytautaspilkauskas@172.18.1.37>
2013-03-14 20:04:08 +0100

    Improved addTask layout

--- src/pl/byd/promand/Team4/AddTaskActivity.java
+++ src/pl/byd/promand/Team4/AddTaskActivity.java
@@ -2,35 +2,62 @@ package pl.byd.promand.Team4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class AddTaskActivity extends SherlockActivity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_task);
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_task);
 
-        String[] assigneeArray = new String[3];
-        assigneeArray[0] = "Assignee";
-        assigneeArray[1] = "Alina";
-        assigneeArray[2] = "Kristjan";
-        
-        String[] typesArray = new String[3];
-        typesArray[0] = "Type";
-        typesArray[1] = "Bug";
-        typesArray[2] = "Feature";
+		String[] assigneeArray = new String[3];
+		assigneeArray[0] = "Assignee";
+		assigneeArray[1] = "Alina";
+		assigneeArray[2] = "Kristjan";
 
-        Spinner typeSpinner = (Spinner)findViewById(R.id.type);
-        ArrayAdapter<String> typeSpinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, R.id.spinner_item, typesArray);
-        typeSpinner.setAdapter(typeSpinnerAdapter);
-        
-        Spinner assigneeSpinner = (Spinner)findViewById(R.id.assignee);
-        ArrayAdapter<String> assigneeSpinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, R.id.spinner_item, assigneeArray);
-        assigneeSpinner.setAdapter(assigneeSpinnerAdapter);
-    }
+		String[] typesArray = new String[3];
+		typesArray[0] = "Type";
+		typesArray[1] = "Bug";
+		typesArray[2] = "Feature";
+
+		Spinner typeSpinner = (Spinner) findViewById(R.id.type);
+		ArrayAdapter<String> typeSpinnerAdapter = new ArrayAdapter<String>(
+				this, R.layout.spinner_item, R.id.spinner_item, typesArray);
+		typeSpinner.setAdapter(typeSpinnerAdapter);
+
+		Spinner assigneeSpinner = (Spinner) findViewById(R.id.assignee);
+		ArrayAdapter<String> assigneeSpinnerAdapter = new ArrayAdapter<String>(
+				this, R.layout.spinner_item, R.id.spinner_item, assigneeArray);
+		assigneeSpinner.setAdapter(assigneeSpinnerAdapter);
+
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				EditText title = (EditText) findViewById(R.id.title);
+				Spinner assignee = (Spinner) findViewById(R.id.assignee);
+				DatePicker deadline = (DatePicker) findViewById(R.id.deadline);
+				Spinner type = (Spinner) findViewById(R.id.type);
+				RadioGroup radioPriority = (RadioGroup) findViewById(R.id.radioPriority);
+				EditText description = (EditText) findViewById(R.id.description);
+
+				String deadlineString = deadline.toString();
+				System.out.println(deadlineString);
+			}
+		});
+
+	}
 }
