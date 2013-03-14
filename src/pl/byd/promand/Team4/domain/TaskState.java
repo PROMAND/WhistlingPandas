3419676971b343d2db4d78359588f4bc3dada66d
francks888 <kristjan.veskimae@gmail.com>
2013-03-15 01:17:03 +0200

    Additional files that weren't pushed

---
+++ src/pl/byd/promand/Team4/domain/TaskState.java
@@ -0,0 +1,16 @@
+package pl.byd.promand.Team4.domain;
+
+public enum TaskState {
+	
+	// Keep in the order as in tasks list view!
+	
+	// In progress
+	IP, 
+	// Assigned
+	A,
+	// Rejected
+	R, 
+	// Finished
+	F;
+
+}
