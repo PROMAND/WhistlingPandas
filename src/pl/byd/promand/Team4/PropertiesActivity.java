3d9dfd96e12310b4b185a6b1cfbfa00af61c1693
francks888 <kristjan.veskimae@gmail.com>
2013-03-22 03:03:37 +0200

    revert

--- src/pl/byd/promand/Team4/PropertiesActivity.java
+++ src/pl/byd/promand/Team4/PropertiesActivity.java
@@ -7,7 +7,6 @@ import pl.byd.promand.Team4.domain.Project;
 import pl.byd.promand.Team4.twitter.AddMemberTweet;
 import pl.byd.promand.Team4.twitter.NewProjectTweet;
 import pl.byd.promand.Team4.utils.MainModel;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -62,7 +61,6 @@ public class PropertiesActivity extends SherlockPreferenceActivity {
                     MainModel.getInstance().sendTweet(newProjectTweet.getTweet());
 
                     members.add(newCreator);
-                    MainModel.getInstance().setYourself(newCreator);
                     if (MainModel.getInstance().getProject() == null)       {
                         Project project = new Project("testname", members);
                     }
@@ -112,8 +110,7 @@ public class PropertiesActivity extends SherlockPreferenceActivity {
 
         Preference button = (Preference)findPreference("goToTheMainPage");
         button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @SuppressLint("NewApi")
-			@Override
+            @Override
             public boolean onPreferenceClick(Preference arg0) {
 
                 Context context = getApplicationContext();
